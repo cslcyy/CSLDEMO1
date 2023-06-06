2a290f546066bbcb31f4dae3ee1d9e86e026d782
CSL <534361008@qq.com>
2023-06-06 09:17:02 +0800

    示例代码

---
+++ Project6/Project6/test6.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+//{
+	//printf("用*号输出字母 C!\n");
+	//printf("****\n");
+	//printf("*\n");
+	//printf("*\n");
+	//printf("****\n");
+	//return 0;
+//}
+
+//下面为举一反三练习
+
+	{
+		printf("用*号输出字母 A!\n");
+		printf(" *\n");
+		printf("***\n");
+		printf("* *\n");
+		return 0;
+	}
