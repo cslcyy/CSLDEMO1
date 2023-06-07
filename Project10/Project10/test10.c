bc8161be5a6f9d08300dd161e96162c73fd0c7bf
CSL <534361008@qq.com>
2023-06-07 08:44:21 +0800

    经典示例

---
+++ Project10/Project10/test10.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	printf("\1\1\n"); //*Êä³öÁ½¸öÐ¦Á³*
+	for (i = 1; i < 11; i++) 
+	{
+		for(j=1;j<=i;j++)
+		printf("%c%c", 219, 219);
+		printf("\n");
+	}
+	
+	return 0;
+}
