5619bb5d201a11d53c7895692271a5fca05f208c
CSL <534361008@qq.com>
2023-06-22 13:18:04 +0800

    test2

--- test2/test2/test2.c
+++ test2/test2/test2.c
@@ -535,3 +535,66 @@
 //
 //	return 0;
 //}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	
+//	again:
+//	printf("helli bit\n");
+//	goto again;
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	//shutdown -s -t 60   在cmd的command里输入会60秒后电脑关机
+//	//shutdown -a      //取消关机
+//	//system()      //执行系统命令的
+//	system("shutdown -s -t 60");
+//again:
+//	printf("请注意，你的电脑在一分钟内关机,如果输入:我是猪,就取消关机\n请输入>:");
+//	scanf("%s", input);
+//	if (strcmp(input, "我是猪") == 0)    //strcmp用来比较两个字符串用的函数
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
+
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char input[20] = { 0 };
+	//shutdown -s -t 60   在cmd的command里输入会60秒后电脑关机
+	//shutdown -a      //取消关机
+	//system()      //执行系统命令的
+	system("shutdown -s -t 60");
+    while(1)
+	{
+		printf("请注意，你的电脑在一分钟内关机,如果输入:我是猪,就取消关机\n请输入>:");
+		scanf("%s", input);
+		if (strcmp(input, "我是猪") == 0)    //strcmp用来比较两个字符串用的函数
+		{
+			system("shutdown -a");
+			break;
+		}
+	}
+	return 0;
+}
